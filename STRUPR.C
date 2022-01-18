7fa90875ad993d7f574785dc345f30a18a4bbd01
albertjoseph2002 <96769506+albertjoseph2002@users.noreply.github.com>
2022-01-18 19:05:31 +0530

    Add files via upload

---
+++ STRUPR.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+	char str1[]="world";
+	clrscr();
+	strupr(str1);
+	printf("string in upper case :\t %s",str1);
+	getch();
+}

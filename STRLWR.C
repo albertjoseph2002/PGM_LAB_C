17a85a399f3f6e31c4214626cca12d4fa67bb081
albertjoseph2002 <96769506+albertjoseph2002@users.noreply.github.com>
2022-01-18 19:03:33 +0530

    Add files via upload

---
+++ STRLWR.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+	char str1[]="HELLO";
+	clrscr();
+	strlwr(str1);
+	printf("string in lower case :\t %s",str1);
+	getch();
+}

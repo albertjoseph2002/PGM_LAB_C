124eb9bc1bf76b7cbf393c87dc452145b6664f3c
albertjoseph2002 <96769506+albertjoseph2002@users.noreply.github.com>
2022-01-18 19:03:11 +0530

    Add files via upload

---
+++ STRLEN.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+	char str1[]="hello";
+	int a;
+	clrscr();
+	a=strlen(str1);
+	printf("length of string is: %d",a);
+	getch();
+}
